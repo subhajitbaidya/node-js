1614d905d491e9e1de5518ac8527ca25454896cd
Subhajit Baidya <subhajitbaidy43@gmail.com>
2024-10-09 08:05:48 +0530

    added url library to parse URL

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const server = http.createServer((request, response) =>{
+    if (request.url === "/favicon.ico") return response.end();
     const log = `${Date.now()}: ${request.url} New request received\n`;
+    const myUrl = url.parse(request.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
-        switch (request.url){
+        switch (myUrl.pathname){
             case "/":
                 response.end("Homepage");
                 break;
             case "/about":
-                response.end("I am Subhajit");
+                const username = myUrl.query.myname;
+                response.end(`Hello, ${username}`);
                 break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                response.end("Here are your search results for "+search);
             default:
                 response.end("404 not found");
         }
